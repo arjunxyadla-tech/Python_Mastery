48aae7e19fedea39abe95e823e7093844e3f8a5d
Arjun Y <arjuny1202>
2026-05-31 16:05:33 +0530

    Initial Commint

---
+++ DataTypes_Variables/types_variables.py
@@ -0,0 +1,11 @@
+this_is_int=1;
+this_is_float=1.0;
+this_is_string="This is a string";
+print(type(this_is_int));
+print(type(this_is_float));
+print(type(this_is_string));
+print(this_is_int);
+print(this_is_float);
+print(this_is_string);
+
+
---
+++ Loops/for_loop.py
@@ -0,0 +1,34 @@
+
+for i in range(5):
+    print(i);
+print("Done with for loop");
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x) 
+print("Done with for loop");
+
+for i in range(1, 5):
+    print(i)
+print("Done with for loop");
+
+for i in range(1, 10, 2):
+    print(i);
+print("Done with for loop");
+
+for i in range(100, 10, -2):
+    print(i);
+print("Done with for loop");
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+print("Done with for loop");
+
+fruits = "Banana"
+for x in fruits:
+  print(x)
+print("Done with for loop");
+
